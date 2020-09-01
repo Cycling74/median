c85216af23464a2c0fc5774496b5771cacbc817b
Alex Norman <xnor@cycling74.com>
2020-08-31 19:38:38 -0700

    made our Float and Long types that are shareable and modifyable

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod class;
 pub mod error;
+pub mod types;
 pub mod wrapper;
 
 #[cfg(test)]
---
+++ src/types.rs
@@ -0,0 +1,115 @@
+use std::cell::UnsafeCell;
+use std::convert::From;
+
+//we assume that f64 and i64 are atomic on the platforms we run on
+//so, Float and Long are wrappers that codify that and allow interior mutability, Send and Sync
+
+#[derive(Default)]
+#[repr(transparent)]
+pub struct Float {
+    value: UnsafeCell<f64>,
+}
+
+#[derive(Default)]
+#[repr(transparent)]
+pub struct Long {
+    value: UnsafeCell<i64>,
+}
+
+impl Float {
+    pub fn new(v: f64) -> Self {
+        Self {
+            value: UnsafeCell::new(v),
+        }
+    }
+    pub fn get(&self) -> f64 {
+        unsafe { *self.value.get() }
+    }
+    pub fn set(&self, v: f64) {
+        unsafe {
+            *self.value.get() = v;
+        }
+    }
+}
+
+impl From<f64> for Float {
+    fn from(v: f64) -> Self {
+        Float::new(v)
+    }
+}
+
+impl Long {
+    pub fn new(v: i64) -> Self {
+        Self {
+            value: UnsafeCell::new(v),
+        }
+    }
+    pub fn get(&self) -> i64 {
+        unsafe { *self.value.get() }
+    }
+    pub fn set(&self, v: i64) {
+        unsafe {
+            *self.value.get() = v;
+        }
+    }
+}
+
+impl From<i64> for Long {
+    fn from(v: i64) -> Self {
+        Long::new(v)
+    }
+}
+
+unsafe impl Send for Float {}
+unsafe impl Sync for Float {}
+unsafe impl Send for Long {}
+unsafe impl Sync for Long {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+
+    #[derive(Default)]
+    pub struct A {
+        pub f: Float,
+    }
+
+    impl A {
+        pub fn new() -> Self {
+            Self {
+                f: Float::new(0f64),
+            }
+        }
+    }
+
+    static BLAH: A = A {
+        f: Float {
+            value: UnsafeCell::new(0f64),
+        },
+    };
+
+    #[test]
+    fn can_from() {
+        let x: Long = 4i64.into();
+        assert_eq!(x.get(), 4i64);
+    }
+
+    #[test]
+    fn can_share() {
+        let x = Arc::new(A::new());
+        (*x).f.set(20f64);
+        BLAH.f.set(1f64);
+        let xc = x.clone();
+        std::thread::spawn(move || {
+            assert_eq!(1f64, BLAH.f.get());
+            assert_eq!(20f64, (*xc).f.get());
+            BLAH.f.set(2f64);
+            (*xc).f.set(10f64);
+        })
+        .join()
+        .unwrap();
+        assert_eq!(2f64, BLAH.f.get());
+        assert_eq!(10f64, (*x).f.get());
+    }
+}
