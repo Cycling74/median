0ddd22c9e4ba7cadab0d32e8a4a58346c46e7ade
Alex Norman <xnor@cycling74.com>
2020-09-19 11:17:28 -0700

    object box, wrapper for max allocated object

--- src/object.rs
+++ src/object.rs
@@ -19,3 +19,53 @@ pub unsafe trait MSPObj: Sized {
 }
 
 unsafe impl<T> MaxObj for T where T: MSPObj {}
+
+use std::ops::{Deref, DerefMut};
+
+/// A smart pointer for an object that max allocated
+pub struct ObjBox<T: MaxObj> {
+    pub value: Option<Box<T>>,
+}
+
+impl<T: MaxObj> ObjBox<T> {
+    pub unsafe fn alloc(class: *mut max_sys::t_class) -> Self {
+        let value = max_sys::object_alloc(class);
+        let value = std::mem::transmute::<_, *mut T>(value);
+        Self::from_raw(value)
+    }
+
+    pub unsafe fn from_raw(value: *mut T) -> Self {
+        Self {
+            value: Some(Box::from_raw(value)),
+        }
+    }
+
+    pub fn into_raw(mut self) -> *mut T {
+        let value = self.value.take().unwrap();
+        Box::into_raw(value)
+    }
+}
+
+impl<T: MaxObj> Deref for ObjBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.value.as_ref().unwrap().deref()
+    }
+}
+
+impl<T: MaxObj> DerefMut for ObjBox<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.value.as_mut().unwrap().deref_mut()
+    }
+}
+
+impl<T: MaxObj> Drop for ObjBox<T> {
+    fn drop(&mut self) {
+        if let Some(v) = self.value.take() {
+            unsafe {
+                max_sys::object_free(std::mem::transmute::<_, _>(v));
+            }
+        }
+    }
+}
