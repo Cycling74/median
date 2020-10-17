cdeb1e8096547279efd626cae1d10af9053fff89
Alex Norman <xnor@cycling74.com>
2020-10-16 17:28:22 -0700

    had screwed up the sigs

--- src/attr.rs
+++ src/attr.rs
@@ -1,7 +1,13 @@
 //! Attributes.
 
 use crate::atom::Atom;
+use std::ffi::c_void;
 use std::os::raw::c_long;
+
+pub type AttrTrampGetMethod<T> =
+    extern "C" fn(s: *mut T, attr: c_void, ac: *mut c_long, av: *mut *mut max_sys::t_atom);
+pub type AttrTrampSetMethod<T> =
+    extern "C" fn(s: *mut T, attr: c_void, ac: c_long, av: *mut *mut max_sys::t_atom);
 
 //p_sym_char (char), _sym_long (long), _sym_float32 (32-bit float), _sym_float64 (64-bit float), _sym_atom (Max t_atom pointer), _sym_symbol (Max t_symbol pointer), _sym_pointer (generic pointer) and _sym_object (Max t_object pointer).
 pub enum AttrType {
@@ -42,7 +48,7 @@ pub enum AttrVisiblity<T> {
     UserVisible(T),
 }
 
-/// handler the boiler plate of dealing with attribute atoms
+/// handle the boiler plate of dealing with attribute atoms
 pub fn get<T, F>(ac: *mut c_long, av: *mut *mut max_sys::t_atom, getter: F) -> max_sys::t_max_err
 where
     F: Fn() -> T,
@@ -63,17 +69,14 @@ where
     max_sys::e_max_errorcodes::MAX_ERR_NONE as _
 }
 
-pub fn set<'a, T, F>(
-    ac: *mut c_long,
-    av: *mut *mut max_sys::t_atom,
-    setter: F,
-) -> max_sys::t_max_err
+/// handle the boiler plate of dealing with attribute atoms
+pub fn set<'a, T, F>(ac: c_long, av: *mut *mut max_sys::t_atom, setter: F) -> max_sys::t_max_err
 where
     F: Fn(T),
     T: From<&'a Atom>,
 {
     unsafe {
-        if *ac > 0 && !(*av).is_null() {
+        if ac > 0 && !av.is_null() && !(*av).is_null() {
             //transparent so this is okay
             let a: &Atom = std::mem::transmute::<_, _>(*av);
             setter(a.into());
