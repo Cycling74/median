85a18067f07bc95c897af8ad4563c0c23448653d
Alex Norman <xnor@cycling74.com>
2020-11-02 08:18:56 -0800

    MSP now works with external macro

--- median/examples/hello_dsp/src/lib.rs
+++ median/examples/hello_dsp/src/lib.rs
@@ -5,113 +5,88 @@ use median::{
     clock::ClockHandle,
     num::Int64,
     post,
-    wrapper::{MSPObjWrapped, MSPObjWrapper, ObjWrapped, WrapperWrapped},
+    wrapper::{
+        attr_get_tramp, attr_set_tramp, tramp, MSPObjWrapped, MSPObjWrapper, WrapperWrapped,
+    },
 };
 
-use std::convert::From;
-
-use std::ffi::c_void;
-use std::os::raw::c_long;
-
-pub struct HelloDSP {
-    pub value: Int64,
-    _v: String,
-    clock: ClockHandle,
-}
-
-impl MSPObjWrapped<HelloDSP> for HelloDSP {
-    fn new(builder: &mut dyn MSPWrappedBuilder<Self>) -> Self {
-        builder.add_signal_inlets(2);
-        builder.add_signal_outlets(2);
-        Self {
-            value: Int64::new(0),
-            _v: String::from("blah"),
-            clock: builder.with_clockfn(Self::clocked),
-        }
+median::external! {
+    #[name="hello_dsp~"]
+    pub struct HelloDSP {
+        pub value: Int64,
+        _v: String,
+        clock: ClockHandle,
     }
 
-    fn perform(&self, _ins: &[&[f64]], outs: &mut [&mut [f64]], _nframes: usize) {
-        for o in outs[0].iter_mut() {
-            *o = 2f64;
-        }
-        for o in outs[1].iter_mut() {
-            *o = 1f64;
-        }
-    }
-
-    /// Register any methods you need for your class
-    fn class_setup(c: &mut Class<MSPObjWrapper<Self>>) {
-        pub extern "C" fn bang_trampoline(w: &MSPObjWrapper<HelloDSP>) {
-            w.wrapped().bang();
+    impl MSPObjWrapped<HelloDSP> for HelloDSP {
+        fn new(builder: &mut dyn MSPWrappedBuilder<Self>) -> Self {
+            builder.add_signal_inlets(2);
+            builder.add_signal_outlets(2);
+            Self {
+                value: Int64::new(0),
+                _v: String::from("blah"),
+                clock: builder.with_clockfn(Self::clocked),
+            }
         }
 
-        pub extern "C" fn int_trampoline(w: &MSPObjWrapper<HelloDSP>, v: i64) {
-            w.wrapped().int(v);
+        fn perform(&self, _ins: &[&[f64]], outs: &mut [&mut [f64]], _nframes: usize) {
+            for o in outs[0].iter_mut() {
+                *o = 2f64;
+            }
+            for o in outs[1].iter_mut() {
+                *o = 1f64;
+            }
         }
 
-        pub extern "C" fn attr_get_trampoline(
-            w: &MSPObjWrapper<HelloDSP>,
-            _attr: c_void,
-            ac: *mut c_long,
-            av: *mut *mut max_sys::t_atom,
-        ) {
-            median::attr::get(ac, av, || w.wrapped().value.get());
-        }
+        /// Register any methods you need for your class
+        fn class_setup(c: &mut Class<MSPObjWrapper<Self>>) {
+            c.add_method(median::method::Method::Int(Self::int_tramp));
+            c.add_method(median::method::Method::Bang(Self::bang_tramp));
 
-        pub extern "C" fn attr_set_trampoline(
-            w: &MSPObjWrapper<HelloDSP>,
-            _attr: c_void,
-            ac: c_long,
-            av: *mut max_sys::t_atom,
-        ) {
-            median::attr::set(ac, av, |v: i64| {
-                post!("attr_set_trampoline {}", v);
-                w.wrapped().value.set(v);
-            });
-        }
-
-        c.add_method(median::method::Method::Int(int_trampoline));
-        c.add_method(median::method::Method::Bang(bang_trampoline));
-
-        c.add_attribute(
-            AttrBuilder::new_accessors(
-                "blah",
-                AttrType::Int64,
-                attr_get_trampoline,
-                attr_set_trampoline,
+            c.add_attribute(
+                AttrBuilder::new_accessors(
+                    "blah",
+                    AttrType::Int64,
+                    Self::blah_tramp,
+                    Self::set_blah_tramp,
+                )
+                .build()
+                .unwrap(),
             )
-            .build()
-            .unwrap(),
-        )
-        .expect("failed to add attribute");
-    }
-}
-
-impl ObjWrapped<HelloDSP> for HelloDSP {
-    fn class_name() -> &'static str {
-        &"hello_dsp~"
-    }
-}
-
-impl HelloDSP {
-    pub fn bang(&self) {
-        post!("from rust {}", self.value);
-        self.clock.delay(10);
+                .expect("failed to add attribute");
+        }
     }
 
-    pub fn int(&self, v: i64) {
-        self.value.set(v);
-        //XXX won't compile, needs mutex
-        //self._v = format!("from rust {}", self.value);
-        post!("from rust {}", self.value);
+    type Wrapper = MSPObjWrapper<HelloDSP>;
+
+    impl HelloDSP {
+        #[tramp(Wrapper)]
+        pub fn bang(&self) {
+            post!("from rust {}", self.value);
+            self.clock.delay(10);
+        }
+
+        #[tramp(Wrapper)]
+        pub fn int(&self, v: i64) {
+            self.value.set(v);
+            //XXX won't compile, needs mutex
+            //self._v = format!("from rust {}", self.value);
+            post!("from rust {}", self.value);
+        }
+
+        #[attr_get_tramp(Wrapper)]
+        pub fn blah(&self) -> i64 {
+            self.value.get()
+        }
+
+        #[attr_set_tramp(Wrapper)]
+        pub fn set_blah(&self, v: i64) {
+            self.value.set(v);
+        }
+
+        pub fn clocked(&self) {
+            post("clocked".to_string());
+        }
     }
 
-    pub fn clocked(&self) {
-        post("clocked".to_string());
-    }
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn ext_main(_r: *mut c_void) {
-    MSPObjWrapper::<HelloDSP>::register(false)
 }
