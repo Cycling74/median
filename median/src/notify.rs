ac58a1870ea79078253f01d41d4f9f6ab6cc7b66
Alex Norman <xnor@cycling74.com>
2020-11-06 08:53:21 -0800

    a notification encapsulation into an object

--- median/src/notify.rs
+++ median/src/notify.rs
@@ -1,9 +1,48 @@
+use crate::symbol::SymbolRef;
 use core::ffi::c_void;
 
 pub type NotifyMethod<T> = unsafe extern "C" fn(
     x: *mut T,
     sender_name: *mut max_sys::t_symbol,
-    msg: *mut max_sys::t_symbol,
+    message: *mut max_sys::t_symbol,
     sender: *mut c_void,
     data: *mut c_void,
 );
+
+pub struct Notification {
+    sender_name: SymbolRef,
+    message: SymbolRef,
+    sender: *mut c_void,
+    data: *mut c_void,
+}
+
+impl Notification {
+    pub fn new(
+        sender_name: *mut max_sys::t_symbol,
+        message: *mut max_sys::t_symbol,
+        sender: *mut c_void,
+        data: *mut c_void,
+    ) -> Self {
+        Self {
+            sender_name: sender_name.into(),
+            message: message.into(),
+            sender,
+            data,
+        }
+    }
+    pub fn sender(&self) -> *mut c_void {
+        self.sender
+    }
+
+    pub fn sender_name(&self) -> &SymbolRef {
+        &self.sender_name
+    }
+
+    pub fn data(&self) -> *mut c_void {
+        self.data
+    }
+
+    pub fn message(&self) -> &SymbolRef {
+        &self.message
+    }
+}
